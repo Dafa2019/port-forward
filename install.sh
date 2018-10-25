ad7aa2850ffda14ab628ecc985fb2c41ba37f55c
raylax <raylax@inurl.org>
2018-10-25 17:01:11 +0800

    添加一键使用脚本

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -f "port-forward.sh" ]; then
+    echo "port-forward.sh already exist, use [./port-forward.sh start] to start"
+    exit
+fi
+
 wget https://raw.githubusercontent.com/raylax/port-forward/master/port-forward.sh
 chmod +x port-forward.sh
 ./port-forward.sh start
